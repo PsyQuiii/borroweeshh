4db465308c9f3a759039f0ee4a6a971402c30d3c
PsyQuiii <neutron2546@gmail.com>
2026-03-16 14:14:15 +0700

    อิอิ

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-function showPage(page){
+function showPage(page) {
 
-document.querySelectorAll(".page").forEach(p=>{
-p.style.display="none"
-})
+    document.querySelectorAll(".page").forEach(p => {
+        p.style.display = "none"
+    })
 
-document.getElementById(page).style.display="block"
+    document.getElementById(page).style.display = "block"
 
 }
 
-function borrowItem(){
+function borrowItem() {
 
-let name = document.getElementById("borrower").value
-let item = document.getElementById("item").value
+    let name = document.getElementById("borrower").value
+    let item = document.getElementById("item").value
 
-alert(name + " ยืม " + item)
+    alert(name + " ยืม " + item)
+
+}
+
+function searchItem() {
+
+    let input = document.getElementById("searchInput").value.toLowerCase()
+
+    let table = document.querySelector("#equipment table")
+
+    let tr = table.getElementsByTagName("tr")
+
+    for (let i = 1; i < tr.length; i++) {
+
+        let td = tr[i].getElementsByTagName("td")[0]
+
+        if (td) {
+
+            let text = td.textContent.toLowerCase()
+
+            tr[i].style.display = text.includes(input) ? "" : "none"
+
+        }
+
+    }
 
 }
